248a7235c173d744667abcc5e25af45957675499
pcusir <pcusir@gmail.com>
2024-11-22 15:19:40 -0500

    feat: added https agent

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,9 +1,16 @@
 import { environment } from "@/environment/environment";
 import axios from "axios";
 import { UserAuthDto } from "@/app/dto/user-auth.dto";
+import * as https from "https";
 
 const apiUrl: string = environment.baseUrl;
 
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false, // Disable SSL verification (development only)
+});
+
 export default async function auth(user: UserAuthDto) {
-  return axios.post(`${apiUrl}/auth/token`, user);
+  return axios.post(`${apiUrl}/auth/token`, user, {
+    httpsAgent,
+  });
 }
